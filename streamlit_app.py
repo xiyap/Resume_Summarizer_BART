ff61588e024514414d62c2d4d678d71abc9c6b53
Ian Yap <140477387+xiyap@users.noreply.github.com>
2024-06-20 22:45:11 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,18 +3,6 @@ from utils import set_background, extract_text_from_pdf, extract_contact_links, 
 
 st.set_page_config(page_title = "Ian's Resume Summarizer")
 # set_background('./streamlit_data/background.jpg')
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
 
 session_state = st.session_state
 
